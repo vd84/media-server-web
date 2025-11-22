b0bb1bd1f39b18f64cef945a1974f406bf6819f5
douglashammarstam <douglas.hammarstam@consid.se>
2025-11-22 15:56:06 +0100

    adds imdb types

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,5 +1,39 @@
 export interface Movie {
-    id: string;
-    title: string;
-    filename: string;
+  id: string;
+  title: string;
+  filename: string;
+  omdbMovie: OmdbMovie;
+}
+
+export interface Rating {
+  Source: string;
+  Value: string;
+}
+
+export interface OmdbMovie {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
 }
